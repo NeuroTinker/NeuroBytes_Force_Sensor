dcf555fd8225760cf6b57e062d829f4bea74ce9b
jrwhite <jrwhite20@gmail.com>
2018-01-30 13:51:01 -0600

    works with common. prep for merge

--- FIRMWARE/src/HAL.h
+++ FIRMWARE/src/HAL.h
@@ -15,8 +15,26 @@
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/adc.h>
+#include <libopencm3/stm32/usart.h>
 
 #include "comm.h"
+
+#define NUM_INPUTS      2
+#define HAS_AXONS       true
+#define HAS_DENDS       false
+#define NUM_AXONS       2
+#define NUM_DENDS       0
+#define COMPLIMENTARY_I(I)  i // not needed because there are no dendrites
+#define IS_EXCITATORY(i)    i // not needed ^^^
+
+#define LPUART1         LPUART1_BASE
+
+
+#define PORT_LPUART1_TX GPIOA
+#define PORT_LPUART1_RX GPIOA
+
+#define PIN_LPUART1_RX  GPIO13
+#define PIN_LPUART1_TX  GPIO14
 
 #define PORT_LED		GPIOA
 #define PIN_R_LED		GPIO1
@@ -39,18 +57,40 @@
 #define PIN_AXON2_EX    GPIO7
 #define PIN_AXON2_IN    GPIO6
 
+#define ACTIVATE_INPUT(I, PIN)   active_input_pins[(I)] = PIN; active_input_tick[(I)] = (read_tick + 2) % 3
+
+extern const uint16_t complimentary_pins[NUM_INPUTS];
+extern const uint32_t complimentary_ports[NUM_INPUTS];
+extern volatile uint16_t active_input_pins[NUM_INPUTS];
+extern uint32_t active_input_ports[NUM_INPUTS];
+extern volatile uint16_t active_output_pins[NUM_INPUTS];
+extern uint32_t active_output_ports[NUM_INPUTS];
+extern volatile uint8_t active_input_tick[NUM_INPUTS];
+
+extern volatile uint8_t dendrite_pulse_flag[NUM_INPUTS];
+extern volatile uint8_t dendrite_ping_flag[NUM_INPUTS];
+
+typedef struct{
+    uint8_t device_type;
+    uint32_t unique_id;
+    uint8_t firmware_version;
+} fingerprint_t;
+
 extern volatile uint8_t main_tick;
 extern volatile uint8_t tick;
 static const uint16_t gamma_lookup[1024];
+static const uint16_t device_id[4];
 
 void systick_setup(int xms);
 void clock_setup(void);
 void gpio_setup(void);
 void tim_setup(void);
+void lpuart_setup(void);
 void LEDFullWhite(void);
 void setLED(uint16_t r, uint16_t g, uint16_t b);
 void setAsInput(uint32_t port, uint32_t pin);
 void setAsOutput(uint32_t port, uint32_t pin);
 void adc_setup(void);
+const uint16_t * getFingerprint(void);
 
 #endif
--- FIRMWARE/src/main.c
+++ FIRMWARE/src/main.c
@@ -5,18 +5,23 @@
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/usart.h>
 
-
-#include "comm.h"
 #include "HAL.h"
+#include "comm.h"
 #include "neuron.h"
 
-#define BLINK_TIME			100
+//#define DBG
+
+#define BLINK_TIME			40
 #define DATA_TIME			10
 #define DEND_PING_TIME		200 // 1000 ms
 #define	NID_PING_TIME		200 // 1000 ms
 #define SEND_PING_TIME		80
 #define BUTTON_PRESS_TIME	2
+#define BUTTON_HOLD_TIME    100
+#define LPUART_SETUP_TIME	100
+#define CHANGE_NID_TIME 	200
 #define ADAPT_TIME			6
 
 static uint32_t fingerprint[3] __attribute__((section (".fingerprint"))) __attribute__ ((__used__)) = {
@@ -27,16 +32,21 @@ static uint32_t fingerprint[3] __attribute__((section (".fingerprint"))) __attri
 
 
 int	adaptation_rate = 0; 
-	int	adaptive_zero = 0;
-	int	sense_input;
-	int	sense_10_bit;
-	int	current_sense;
-	int	zero = 0; // 165 circle
-	int	span = 10000; // 3000 circle
-	int	fire_time = 0;
-	int	fire_led = 0;
-	int	zero_freq = 150;
-	int adapt_time = 0;
+int	adaptive_zero = 0;
+int	sense_input;
+int	sense_10_bit;
+int	current_sense;
+int	zero = 0; // 165 circle
+int	span = 10000; // 3000 circle
+int	fire_time = 0;
+int	fire_led = 0;
+int	zero_freq = 150;
+int adapt_time = 0;
+
+typedef enum{
+	CURRENT		=	0b0001,
+	ADAPT 		=	0b0010
+} parameter_identifiers;
 
 int scale10bit(int val, int zero, int span) 
 {
@@ -75,8 +85,18 @@ int main(void)
 	uint8_t		button_armed = 0;
 	uint16_t	button_status = 0;
 	uint32_t	nid_channel = 0b000; //debug
-	uint32_t	message = 0;
+	uint8_t		fire_flag = 0;
+	uint8_t		fire_tick = 0;
+	uint16_t	lpuart_setup_time = 0;
+	uint8_t		change_nid_time = 0;
 
+	int16_t fire_data = 0;
+	uint16_t fire_delay_time = 0;
+	uint16_t fire_delay_time_reset = FIRE_DELAY_TIME;
+	fire_delay_time_reset = 40; // temporary
+	uint16_t fire_delay_overflow = 0;
+
+	message_t message;
 
 	neuron_t 	neuron;
 	uint8_t		i;
@@ -89,21 +109,19 @@ int main(void)
 	tim_setup();
 	gpio_setup();
 	adc_setup();
-	setLED(200,0,0);
 
-	//	MMIO32(SYSCFG_BASE + 0x0c) = 0b1111 << 12;
-
+	#ifdef DBG
+	blink_flag = 1;
+	#endif
 	
 	for(;;)
 	{
-		/*
-		adc_start_conversion_regular(ADC1);
-		while (!(adc_eoc(ADC1)));
-		sense_input = adc_read_regular(ADC1);
-		sense_10_bit = scale10bit(sense_input, zero, span);
 		
-	setLED(sense_10_bit,sense_10_bit,sense_10_bit);
-	*/
+		// adc_start_conversion_regular(ADC1);
+		// while (!(adc_eoc(ADC1)));
+		// sense_input = adc_read_regular(ADC1);
+		// sense_10_bit = scale10bit(sense_input, zero, span);
+		
 		if (main_tick == 1){
 			// 5 ms
 			main_tick = 0;
@@ -115,6 +133,7 @@ int main(void)
 			adaptation_rate = 0;
 			current_sense = sense_10_bit - adaptive_zero;
 
+			adapt_time = 0; // debug
 			if (++adapt_time == ADAPT_TIME){
 				adapt_time = 0;
 				if (adaptation_rate != 0){
@@ -125,61 +144,120 @@ int main(void)
 					}
 				}
 			}
-
-			if (nid_ping_time == 0){
-				nid_keep_alive = NID_PING_KEEP_ALIVE;
-				nid_pin = 0;
-				nid_pin_out = 0;
-			}else {
+			
+			// check to see if nid ping hasn't been received in last NID_PING_TIME ticks
+			if (nid_ping_time > 0){
 				nid_ping_time -= 1;
+				if (nid_ping_time == 0){
+					// nid no longer connected
+					nid_distance = 100; // reset nid_keep_alive
+					nid_pin = 0; // clear the nid pin
+					nid_pin_out = 0;
+					nid_i = NO_NID_I;
+				}
+			}
+
+			if (lpuart_setup_time < LPUART_SETUP_TIME){
+				lpuart_setup_time += 1;
+			} else if (lpuart_setup_time == LPUART_SETUP_TIME){
+				lpuart_setup_time += 1;
+				#ifndef DBG
+				lpuart_setup();
+				#endif
 			}
 			
+			if (change_nid_time++ > CHANGE_NID_TIME){
+				change_nid_time = 0;
+				closer_distance = nid_distance;
+				closer_ping_count = 0;
+			}
+			
+			// send a downstream ping every SEND_PING_TIME ticks
 			if (send_ping_time++ > SEND_PING_TIME){
-				addWrite(DOWNSTREAM_BUFF, DEND_PING);
-				//downstream_write_buffer = DEND_PING;
-				//downstream_write_buffer_ready = 1;
+				// send downstream ping through axon
+				addWrite(DOWNSTREAM_BUFF, downstream_ping_message);
 				send_ping_time = 0;
 			}
 
-			if (identify_time > 0){
-				identify_time -= 1;
+
+			if (comms_flag != 0){
+				switch (comms_flag){
+					case ADAPT:
+						adaptation_rate = comms_data;	
+						break;
+					default:
+						break;
+				}
+				comms_flag = 0;
+				comms_data = 0;
 			}
 
-			button_status = gpio_get(PORT_IDENTIFY, PIN_IDENTIFY);
-			button_status >>= 3;
-			button_status &= 0b1;
-
-			if (button_status == 0){ // !=
-				if (button_press_time++ >= BUTTON_PRESS_TIME){
-					button_armed = 1;
-					button_press_time = 0;
+			// check for clear channel command
+			if (identify_time < IDENTIFY_TIME){
+				if (identify_time == 0){
+					if ((identify_channel == 0) || (identify_channel == nid_channel)){
+						nid_channel = 0;
+					}
 				}
-			} else if (button_armed == 1){
-				if (identify_time > 0){
+				identify_time += 1;
+			}
+			
+			// check identify button
+			button_status = gpio_get(PORT_IDENTIFY, PIN_IDENTIFY);
+
+			// if identify button is pressed and identify_time < IDENTIFY_TIME (i.e. NID sent 'identify'' message), set new nid_channel
+			if (button_status == 0){
+				// debounce
+				button_press_time += 1;
+				if (button_press_time >= BUTTON_HOLD_TIME){
+					button_armed = 2;
+					blink_flag = 1;
+				} else if (button_press_time >= BUTTON_PRESS_TIME){
+					button_armed = 1;
+				}
+			} else{
+				// button not pressed
+				if (button_armed == 0){
+					button_press_time = 0;
+				} else if (button_armed == 1 && nid_i != NO_NID_I){
 					nid_channel = identify_channel;
-				} else{
+					identify_time = 1;
+					button_armed = 0;
+				} else if (button_armed == 2){
 					if (adaptation_rate > 0){
 						adaptation_rate = 0;
 						adaptive_zero = 0;
 					} else{
 						adaptation_rate = 10;
 					}
+					button_armed = 0;
 				}
-				button_armed = 0;
-			} else{
 				button_press_time = 0;
 			}
-
-			adaptation_rate = 0;
 			
+			// send current membrane potential to NID if currently identified by NID
 			if (nid_channel != 0){
+				// send data every DATA_TIME ticks
 				if (data_time++ > DATA_TIME){
+					if (fire_data > 0){
+						if (fire_data == 1){
+							message.message = (((uint32_t) DATA_MESSAGE)) | ((uint16_t) HYPERPOLARIZATION);
+							fire_data = 0;						
+						} else {
+							message.message = (((uint32_t) DATA_MESSAGE)) | ((uint16_t) fire_data);	
+							fire_data = 1;						
+						}
+					} else {
+						message.message = (((uint32_t) DATA_MESSAGE)) | ((uint16_t) neuron.potential);						
+					}
 					data_time = 0;
-					message = DATA_MESSAGE | (uint16_t) current_sense | (nid_channel << 19) | (nid_keep_alive << 22);
-					addWrite(NID_BUFF,message);
+					message.length = 32;
+					message.message |= (nid_channel << 21);
+					addWrite(NID_BUFF,(const message_t) message);
 				}
 			}
-			neuron.leaky_current = sense_10_bit * 11 / 10;
+
+			neuron.leaky_current = sense_10_bit * 9 / 10;
 			membraneDecayStep(&neuron);
 			neuron.potential = 0;
 			neuron.potential += neuron.fire_potential;
@@ -191,8 +269,28 @@ int main(void)
 				neuron.state = FIRE;
 				neuron.fire_potential = HYPERPOLARIZATION;
 				neuron.fire_time = PULSE_LENGTH;
-				// send downstream pulse
-				addWrite(DOWNSTREAM_BUFF, PULSE_MESSAGE);
+
+				fire_data = neuron.potential;
+
+				if (fire_delay_time == 0) {
+					fire_delay_time = fire_delay_time_reset;
+					fire_delay_overflow = 0;
+				} else {
+					fire_delay_overflow = fire_delay_time_reset - fire_delay_time;
+				}
+
+				fire_flag = 1;
+			} else if (neuron.potential < HYPERPOLARIZATION) {
+				neuron.potential = HYPERPOLARIZATION;
+			}
+
+			if (fire_delay_time > 0){
+				fire_delay_time -= 1;
+			} else if (fire_flag == 1){
+				if (fire_delay_overflow > 0)
+					fire_delay_time = fire_delay_overflow; // TODO: make this an actual FIFO buffer of fire timings
+				fire_flag = 0;
+				addWrite(DOWNSTREAM_BUFF, pulse_message);
 			}
 			
 			if (blink_flag != 0){
@@ -201,7 +299,7 @@ int main(void)
 				blink_flag = 0;
 			} else if (blink_time > 0){
 				if (++blink_time == BLINK_TIME){
-					setLED(200,0,0);
+					setLED(200,0,200);
 					blink_time = 0;
 				}
 			}else if (neuron.state == FIRE){
